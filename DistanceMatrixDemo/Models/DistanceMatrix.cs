2c3614bc9270e71ead0fa3eec9bbb34429177114
David Gullmarsvik <david.gullmarsvik@gmail.com>
2013-05-22 08:10:22 +0200

    uppgift 2 forts

--- DistanceMatrixDemo/Models/DistanceMatrix.cs
+++ DistanceMatrixDemo/Models/DistanceMatrix.cs
@@ -5,6 +5,14 @@ using System.Web;
 
 namespace DistanceMatrixDemo.Models
 {
+    public class DistanceMatrixResult
+    {
+        public List<String> destination_addresses {get;set;}
+        public List<String> origin_addresses {get; set;}
+        public List<DistanceMatrixRow> rows { get;set;}
+        public string status { get; set; }
+    }
+
     public class DistanceMatrix
     {
     }
